6ecac5706f5a6352c8ddc554b7989fa75c126f7b
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2021-07-15 15:38:32 +0200

    Added pot file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cf7dede90771f8f319780217c0a5cce3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '95b2ac5f5ef5679349d5857be103d840');
